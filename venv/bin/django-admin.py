daf4114692ea488f4d4e38c9eff2b1c80042f976
EossForos <volosonopulos@gmail.com>
2020-02-16 14:50:38 +0200

    first commit

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/vlassis/Public/django_projects/Python_Scripts/Django_Projects/django_movie/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
